f7e7de634436e41651f7105d9b2bf264b1e7f685
Ferra0612 <saintsimonferdine0612@gmail.com>
2025-06-17 18:41:30 -0400

    Add files via upload

---
+++ Projet final Script.js
@@ -0,0 +1,16 @@
+﻿document.getElementById("contactForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const name = document.getElementById("name").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const message = document.getElementById("message").value.trim();
+    const status = document.getElementById("form-status");
+
+    if (name && email && message) {
+        status.textContent = "Message envoyé avec succès ✔";
+        this.reset();
+    } else {
+        status.textContent = "Veuillez remplir tous les champs.";
+        status.style.color = "red";
+    }
+});
